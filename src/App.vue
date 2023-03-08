a832de1761c9a47fb25ce2eed85985cc560d546b
brianmujjuni <brianmujjuni@engineer.com>
2023-03-08 12:06:08 +0300

    pinia first stracture

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="" />
+      <h1>Pinia Tasks</h1>
+    </header>
+   
+    
+  </main>
+</template>
+
+<script>
+import { useTaskStore } from "./stores/TaskStore";
+export default {
+  setup() {
+    const taskStore = useTaskStore();
+
+    return { taskStore };
+  },
+};
+</script>
