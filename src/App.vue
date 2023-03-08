747f211b32e559ecbea1a87dbc210fdbebad0ae9
brianmujjuni <brianmujjuni@engineer.com>
2023-03-08 13:06:59 +0300

    added gatters

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,39 @@
       <img src="./assets/pinia-logo.svg" alt="" />
       <h1>Pinia Tasks</h1>
     </header>
-   
+   <!--filter-->
+    <nav class="filter">
+      <button @click="filter = 'all'">All tasks</button>
+      <button @click="filter = 'favs'">Fav tasks</button>
+    </nav>
+
+    <div class="task-list" v-if="filter  === 'all'">
+      <p>you have {{ taskStore.totalCount }} tasks</p>
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task"/> 
+      </div>
+    </div>
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>favorites</p>
+      <div v-for="task in taskStore.favs">
+        <TaskDetails :task="task"/> 
+      </div>
+    </div>
     
+
   </main>
 </template>
 
 <script>
+import { ref } from "vue";
+import TaskDetails from "./components/TaskDetails.vue";
 import { useTaskStore } from "./stores/TaskStore";
 export default {
+  components:{TaskDetails},
   setup() {
     const taskStore = useTaskStore();
-
-    return { taskStore };
+    const filter = ref('all')
+    return { taskStore,filter };
   },
 };
 </script>
--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -10,6 +10,6 @@
 
 <script>
   export default {
-    props: ['task'],
+    props: ['task']
   }
 </script>
